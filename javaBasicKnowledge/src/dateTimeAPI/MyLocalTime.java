8e3849bc4f9db6ccac31e11f57a2e6657b062f2e
raffi <raffi.hakhverdyan@spiritofsoft.com>
2023-05-12 14:44:58 +0400

    java basic knowledge -for interview questions --LocalTime

---
+++ javaBasicKnowledge/src/dateTimeAPI/MyLocalTime.java
@@ -0,0 +1,70 @@
+package dateTimeAPI;
+
+import java.time.LocalTime;
+
+/*
+hh:mm:ss.zzz
+*/
+public class MyLocalTime {
+
+
+    public static void main(String[] args) {
+
+    /*
+    plusHours(long hours), plusMinutes(long minutes), plusSeconds(long seconds):
+     Adds the specified number of hours, minutes, or seconds to the current time and returns a new LocalTime object.
+    minusHours(long hours), minusMinutes(long minutes), minusSeconds(long seconds):
+     Subtracts the specified number of hours, minutes, or seconds from the current time and returns a new LocalTime object.
+    getHour(), getMinute(), getSecond():
+     Retrieves the hour, minute, and second components of a LocalTime.
+    */
+
+        LocalTime currentTime = LocalTime.now();
+        System.out.println("Current time: " + currentTime);
+
+        // Create a LocalTime object for a specific time
+        LocalTime specificTime = LocalTime.of(10, 30, 15);
+        System.out.println("Specific time: " + specificTime);
+
+        // Retrieve individual components of a time
+        int hour = specificTime.getHour();
+        int minute = specificTime.getMinute();
+        int second = specificTime.getSecond();
+        System.out.println("Hour: " + hour);
+        System.out.println("Minute: " + minute);
+        System.out.println("Second: " + second);
+
+        // Perform operations on LocalTime objects
+        LocalTime futureTime = specificTime.plusHours(2);
+        System.out.println("Future time: " + futureTime);
+
+        LocalTime pastTime = specificTime.minusMinutes(15);
+        System.out.println("Past time: " + pastTime);
+
+        System.out.println("----------------------------------");
+
+        LocalTime _currentTime = LocalTime.now();
+        LocalTime _specificTime = LocalTime.of(15, 30);
+        LocalTime _pastTime = LocalTime.of(12, 0);
+        LocalTime _futureTime = LocalTime.of(18, 45);
+
+        System.out.println("Current Time: " + _currentTime);
+        System.out.println("Specific Time: " + _specificTime);
+        System.out.println("Past Time: " + _pastTime);
+        System.out.println("Future Time: " + _futureTime);
+
+        // Comparing times using isEqual()
+        System.out.println("Is specificTime equal to currentTime? " + _specificTime.equals(_currentTime));
+        System.out.println("Is specificTime equal to specificTime? " + _specificTime.equals(_specificTime));
+
+        // Comparing times using isBefore()
+        System.out.println("Is specificTime before currentTime? " + _specificTime.isBefore(_currentTime));
+        System.out.println("Is specificTime before pastTime? " + _specificTime.isBefore(_pastTime));
+        System.out.println("Is specificTime before futureTime? " + _specificTime.isBefore(_futureTime));
+
+        // Comparing times using isAfter()
+        System.out.println("Is specificTime after currentTime? " + _specificTime.isAfter(_currentTime));
+        System.out.println("Is specificTime after pastTime? " + _specificTime.isAfter(_pastTime));
+        System.out.println("Is specificTime after futureTime? " + _specificTime.isAfter(_futureTime));
+    }
+}
