cd671b4cc5d91c9c83a9aac69360505034482c54
raffi <raffi.hakhverdyan@spiritofsoft.com>
2023-05-15 17:27:59 +0400

    java basic knowledge -deadlock

--- javaBasicKnowledge/src/forInterviewQuestions/Deadlock.java
+++ javaBasicKnowledge/src/forInterviewQuestions/Deadlock.java
@@ -1,11 +1,23 @@
 package forInterviewQuestions;
 
+import java.util.Comparator;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 /*
 forInterviewQuestions.Deadlock=Փակուղի
 */
 public class Deadlock {
     public static void main(String[] args) {
+
         createDeadlockSituation();
+
+        //solveDeadlockProblem
+//        Object lock3 = new Object();
+//        Object lock4 = new Object();
+//        ExecutorService _solveDeadlockProblem = Executors.newFixedThreadPool(2);
+//        _solveDeadlockProblem.execute(new Thread(() -> solveDeadlockProblem(lock3, lock4),"Thread_3"));
+//        _solveDeadlockProblem.execute(new Thread(() -> solveDeadlockProblem(lock4, lock3),"Thread_4"));
     }
 
     private static void createDeadlockSituation() {
@@ -16,7 +28,7 @@ public class Deadlock {
             System.out.println(Thread.currentThread().getName() + "\t START");
             synchronized (lock1) {
                 try {
-                    Thread.sleep(100);
+                    Thread.sleep(500);
                 } catch (InterruptedException e) {
                     throw new RuntimeException(e);
                 }
@@ -36,6 +48,28 @@ public class Deadlock {
 
         thread1.start();
         thread2.start();
+    }
+
+
+    private static void solveDeadlockProblem(Object res1, Object res2) {
+        Object lock1 = res1;
+        Object lock2 = res2;
+        if (res1.hashCode() < res2.hashCode()) {
+            lock2 = res1;
+            lock1 = res2;
+        }
+
+        System.out.println(Thread.currentThread().getName() + " start");
+        synchronized (lock1) {
+            try {
+                Thread.sleep(500);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            synchronized (lock2) {
+            }
+        }
+        System.out.println(Thread.currentThread().getName() + " end");
     }
 
 }
